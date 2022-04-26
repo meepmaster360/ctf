4873c8094526f27bf99a923406b3e180270cc1ad
meepmaster360 <paulomotaborges@gmail.com>
2022-04-26 22:24:21 +0100

    1

--- nmap.sh
+++ nmap.sh
@@ -81,10 +81,45 @@ function IP () {
 	echo -e "\n$g[!]$e Your IP is: $IP_SISTEMA"
 }
 
+function app_install () {
+
+# Nikto installation	
+
+	if [ ! -x "$(command -v nmap)" ];then
+        echo "[+] nmap not detected...Installing"
+        sudo apt-get install nmap -y > installing;rm installing
+else
+    echo "[+] nmap detected"
+     
+fi
+
+# Nikto installation
+
+if [ ! -x "$(command -v nikto)" ];then
+        echo "[+] nikto not detected...Installing"
+        sudo apt-get install nikto -y > installing;rm installing
+else
+    echo "[+] nikto detected"
+     
+fi
+
+# Dirbuster installation
+
+if [ ! -x "$(command -v dirbuster)" ];then
+        echo "[+] dirbuster not detected...Installing"
+        sudo apt-get install dirbuster -y > installing;rm installing
+else
+    echo "[+] dirbuster detected"
+     
+fi
+}
+
+
 # Call the functions
 
 user
 connect
+app_install
 update_upgrade
 IP
 
